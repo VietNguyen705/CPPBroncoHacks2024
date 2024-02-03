487218915d875abbaf865e96a2fb34786d3607fc
Leo Jiang <leojiang@b98-aruba-authenticated-10-110-191-196.wlan.cpp.edu>
2024-02-02 20:19:58 -0800

    create/retrieve items

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const User = require('./models/User');
 const jwt = require('jsonwebtoken');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // MongoDB Connection
 const uri = process.env.MONGODB_URI;
@@ -77,7 +77,69 @@ app.post('/signup', async (req, res) => {
     res.status(500).send('Error signing up user.');
   }
 });
+// Create items  ** Only Users in the database can create items **
+app.post('/item/create', async (req, res) => {
+  try{  
+    // Access data from the request body
+    const {title, description, price, category, sellerId, 
+        images, createdAt, updatedAt} = req.body;
+    
+    // Check for required fields
+    if (!title || !description || !price || !category || !sellerId || !images) {
+        return res.status(400).json({ error: 'Please fill out all required fields.' });
+    }    
 
+    // Process the data (e.g., save to a database)
+    const newItem = new Item({
+        title,
+        description,
+        price,
+        category,
+        images,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    });
+
+    const savedItem = await newItem.save();
+
+    }  catch {
+        res.status(500).send('Error creating item.');
+    }
+    // Send a response back to the client
+    res.json({ message: 'Item created!' });
+  });
+
+// Retrieve items 
+app.get('/items/:itemId', async (req, res) => {
+    try {
+      const itemId = req.params.itemId;
+  
+      // Fetch the item from the database based on the itemId
+      const foundItem = await Item.findById(itemId);
+  
+      // Check if the item was found
+      if (!foundItem) {
+        return res.status(404).json({ error: 'Item not found.' });
+      }
+  
+      // Send the found item as the response
+      res.json(foundItem);
+    } catch (error) {
+      console.error('Error retrieving item:', error);
+      // Handle other errors and send an appropriate response
+      res.status(500).json({ error: 'Internal server error.' });
+    }
+  });
+/*
+// Partial update with PATCH
+app.patch('/items/:itemId', async (req, res) => {
+    // Apply partial modifications to the item in the database using the provided data
+    
+    
+
+    res.json({ message: 'Item updated successfully!' });
+  });
+*/
 // Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
